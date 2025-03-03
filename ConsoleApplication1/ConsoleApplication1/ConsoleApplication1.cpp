7a1c94a35dfd0e69b3657dd9063ace74f37dfb89
Mykhailo Buhera <mihapvp1990@gmail.com>
2025-03-03 18:25:07 +0200

    add move constructor

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -45,6 +45,14 @@ public:
 
     Course(string name, string code, int cr) : courseName(name), courseCode(code), credut(cr) {}
 
+    // Move constructor
+    Course(Course&& other) noexcept //— це специфікатор у C++, який вказує, що функція або метод не викидає винятків.
+        : courseName(move(other.courseName)),
+        courseCode(move(other.courseCode)),
+        credut(other.credut) {
+        cout << "Move constructor dla course " << courseName << " vuklukano" << endl;
+    }
+
     ~Course() {
         cout << "destructor dla course " << courseName << " vuklukano" << endl;
     }
@@ -64,13 +72,18 @@ public:
     // counstrructor
     Grade(string sID, string cCode, int g) : studentID(sID), courseCode(cCode), grade(g) {}
 
+
+
     //copy
     Grade() : studentID{ "none" }, courseCode{ "none" }, grade{0} {};
     Grade(const Grade& other) {
-        studentID = other.studentID;
-        courseCode = other.courseCode;
-        grade = other.grade;
+        this->studentID = other.studentID;
+        this->courseCode = other.courseCode;
+        this->grade = other.grade;
     }
+
+
+
     // destructor
     ~Grade() {
         cout << "destructor dla ochinok " << studentID << "za kursom " << courseCode << " vuklukano" << endl;
@@ -85,18 +98,21 @@ public:
 
     int main()
     {
-       /* Student student1("ivan", 18, "ch250605");
+        Student student1("ivan", 18, "ch250605");
         student1.display();
 
-        Course course1("Software engennering", "CS143 ", 5);
-        course1.display();
-        */
+    
         Grade grade1("ch250605", "CS143 ", 95);
         grade1.display();
         Grade grade2{};
         grade2 = grade1;
         grade2.display();
 
+        Course course1("Software Engineering", "CS143", 5);
+        course1.display();
+
+        Course course2 = move(course1);
+        course2.display();
 
         return 0;
 
